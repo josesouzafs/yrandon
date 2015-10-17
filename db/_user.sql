631b0fdd413f90750b84ebd792df2b31e998be29
astfarias <astfarias@gmail.com>
2015-10-17 00:35:06 -0300

    First Commit

---
+++ db/_user.sql
@@ -0,0 +1,9 @@
+﻿
+CREATE TABLE _user
+(
+  id_user serial NOT NULL,
+  name_user character varying NOT NULL,
+  e_mail character varying NOT NULL,
+  password character varying(256)[] NOT NULL,
+  CONSTRAINT _user_pkey PRIMARY KEY (id_user)
+)
